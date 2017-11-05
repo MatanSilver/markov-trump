4cb3fedc1c9ace3698c7fdf60af5af463b6d3bec
MatanSilver <matansilver@gmail.com>
2017-11-05 13:07:53 -0500

    changed name to app.py and added click support, as well as a more persistent markov model

---
+++ app.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+import markovify
+import json
+import os
+import flask
+import click
+import wget
+import zipfile
+import requests
+
+def load_json():
+    with open("./master_2016.json", 'r') as f:
+        jsontext = f.read()
+        trumpdict = json.loads(jsontext)
+        return "".join(["{}\n".format(tweet["text"]) for tweet in trumpdict])
+
+def pull_json():
+    url = "https://github.com/bpb27/trump_tweet_data_archive/raw/master/master_2016.json.zip"
+    filename = wget.download(url)
+    zip_ref = zipfile.ZipFile(filename, 'r')
+    zip_ref.extractall(".")
+    zip_ref.close()
+
+app = flask.Flask(__name__)
+if not os.path.isfile("./master_2016.json"):
+    pull_json()
+fulltext = load_json()
+text_model = markovify.Text(fulltext)
+
+@app.route("/generate")
+def generate():
+    return text_model.make_short_sentence(140)
+
+@click.group()
+def cli():
+    pass
+
+@click.command()
+def runserver():
+    app.run()
+
+cli.add_command(runserver)
+
+if __name__ == "__main__":
+    cli()
+
+
